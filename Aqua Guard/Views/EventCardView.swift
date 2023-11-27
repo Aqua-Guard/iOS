34bced7951da913fb79df332ce7d82149615ebd0
malek-labidi <labidi.malek@esprit.tn>
2023-11-27 10:25:30 -0800

    implement card view event design

---
+++ Aqua Guard/Views/EventCardView.swift
@@ -0,0 +1,18 @@
+//
+//  EventCardView.swift
+//  Aqua Guard
+//
+//  Created by Malek Labidi on 27/11/2023.
+//
+
+import SwiftUI
+
+struct EventCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventCardView()
+}
