baa13c733b3b1d4b6ed4a92f329976299a08ecd3
malek-labidi <labidi.malek@esprit.tn>
2023-11-27 03:27:24 -0800

    implmentt splashh screen 1 view design

--- Aqua Guard/SplashScreenView.swift
+++ Aqua Guard/SplashScreenView.swift
@@ -8,11 +8,50 @@
 import SwiftUI
 
 struct SplashScreenView: View {
+    @State private var isActive = false
+    @State private var size = 0.8
+    @State private var opacity = 0.5
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("background_splash_screen")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+            VStack {
+            
+                
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 400, height: 400) // Adjust the size as needed
+                
+                
+            }
+        }
+        .scaleEffect(size)
+        .opacity(opacity)
+        .onAppear {
+            withAnimation(.easeIn(duration: 1.2)) {
+                self.size = 0.9
+                self.opacity = 1.0
+            }
+        }
+        .onAppear{
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0){
+                self.isActive = true
+            }
+        }
     }
 }
 
-#Preview {
-    SplashScreenView()
+#if DEBUG
+struct SplashScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreenView()
+    }
 }
+#endif
+
