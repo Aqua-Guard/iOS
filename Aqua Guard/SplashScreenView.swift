b0a14b287d8df2b37cd758da2d8d38a87c1537e3
malek-labidi <labidi.malek@esprit.tn>
2023-11-27 03:26:58 -0800

    implement splash screen view design

---
+++ Aqua Guard/SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  Aqua Guard
+//
+//  Created by Malek Labidi on 27/11/2023.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
