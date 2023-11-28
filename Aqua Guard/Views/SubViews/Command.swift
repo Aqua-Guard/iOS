b281f2767d62b06a27cdc77ee799491d37522fc9
Youssef Farhat <youssef@MacBook-Pro-de-Youssef.local>
2023-11-28 17:32:33 +0100

    Implement Drawwer Menu

---
+++ Aqua Guard/Views/SubViews/Command.swift
@@ -0,0 +1,18 @@
+//
+//  Command.swift
+//  Aqua Guard
+//
+//  Created by Youssef Farhat on 28/11/2023.
+//
+
+import SwiftUI
+
+struct Command: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Command()
+}
---
+++ Aqua Guard/Views/SubViews/Notification.swift
@@ -0,0 +1,18 @@
+//
+//  Notification.swift
+//  Aqua Guard
+//
+//  Created by Youssef Farhat on 28/11/2023.
+//
+
+import SwiftUI
+
+struct Notification: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Notification()
+}
