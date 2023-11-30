e9c93528df0894e2df10dc281d1a2373691818e0
ademseddik <adem.seddik@esprit.tn>
2023-12-01 00:28:18 +0100

    actualite views

---
+++ Aqua Guard/Views/Actualite/ActualiteCardView.swift
@@ -0,0 +1,18 @@
+//
+//  ActualiteCardView.swift
+//  Aqua Guard
+//
+//  Created by adem on 1/12/2023.
+//
+
+import SwiftUI
+
+struct ActualiteCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActualiteCardView()
+}
