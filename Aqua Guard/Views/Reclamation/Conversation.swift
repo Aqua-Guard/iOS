68681d64fee7996c1af8b984e97fbda8c27606d1
ademseddik <adem.seddik@esprit.tn>
2023-12-01 07:56:15 +0100

    reclamation and conversation

---
+++ Aqua Guard/Views/Reclamation/Conversation.swift
@@ -0,0 +1,18 @@
+//
+//  Conversation.swift
+//  Aqua Guard
+//
+//  Created by adem on 1/12/2023.
+//
+
+import SwiftUI
+
+struct Conversation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Conversation()
+}
