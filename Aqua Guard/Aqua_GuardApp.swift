f5f5c02418adf91cdef99e61f370b5fcc5061e1b
youssef <youssef.farhat@esprit.tn>
2023-12-03 20:47:03 +0100

    amira update user

--- Aqua Guard/Aqua_GuardApp.swift
+++ Aqua Guard/Aqua_GuardApp.swift
@@ -13,10 +13,10 @@ struct Aqua_GuardApp: App {
 
     var body: some Scene {
         WindowGroup {
-             ContentView()
+            // ContentView()
            // Home()
                 //.environment(\.managedObjectContext, persistenceController.container.viewContext)
-           // SplashScreenView()
+            SplashScreenView()
 
         }
     }
