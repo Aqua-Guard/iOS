e16ee71920ae1724ba2cc989ea23195cb167dd79
AmiraBenMbarek <amira.benmbarek@esprit.tn>
2024-01-04 00:53:58 +0100

    update profile

--- Aqua Guard/Views/AccountScreen.swift
+++ Aqua Guard/Views/AccountScreen.swift
@@ -20,101 +20,101 @@ struct AccountScreen: View {
             ZStack{
                 LinearGradient(gradient: Gradient(colors: [isDarkModeEnabled ? Color.black : Color.white, isDarkModeEnabled ? Color.lightBlue : Color.lightBlue]), startPoint: .top, endPoint: .bottom)
                     .edgesIgnoringSafeArea(.all)
-                
-                VStack {
-                    Spacer()
-                    
-                    VStack {
-                        HStack {
-                            Button(action: {
-                                self.presentationMode.wrappedValue.dismiss()
-                            }) {
-                                Image(systemName: "arrow.left.circle.fill")
-                                    .resizable()
-                                    .foregroundColor(.blue)
-                                    .frame(width: 25, height: 25)
-                            }
-                            
-                            Spacer()
-                        }
-                    }
-                    .alignmentGuide(.top) { _ in 0 }
-                    .alignmentGuide(.leading) { _ in 0 }
-                    .padding()
-                            
-                            
+                ScrollView{
+                    VStack{
+                        Spacer()
                         
-                            let userImageURLString = "https://aquaguard-tux1.onrender.com/images/user/\((LoginViewModell.defaults.string(forKey: "image") ?? ""))"
-                            
-                            GeometryReader { geometry in
-                                AsyncImage(url: URL(string: userImageURLString)) { phase in
-                                    switch phase {
-                                    case .success(let image):
-                                        image
-                                            .resizable()
-                                            .scaledToFit()
-                                            .frame(width: geometry.size.width, height: geometry.size.height)
-                                            .clipShape(Circle())
-                                            .overlay(Circle().stroke(Color.white, lineWidth: 2))
-                                    @unknown default:
-                                        EmptyView()
-                                    }
+                        VStack {
+                            HStack {
+                                Button(action: {
+                                    self.presentationMode.wrappedValue.dismiss()
+                                }) {
+                                    Image(systemName: "arrow.left.circle.fill")
+                                        .resizable()
+                                        .foregroundColor(.blue)
+                                        .frame(width: 25, height: 25)
+                                }
+                                
+                                Spacer()
+                            }
+                        }
+                        .alignmentGuide(.top) { _ in 0 }
+                        .alignmentGuide(.leading) { _ in 0 }
+                        .padding()
+                        
+                        
+                        
+                        let userImageURLString = "https://aquaguard-tux1.onrender.com/images/user/\((LoginViewModell.defaults.string(forKey: "image") ?? ""))"
+                        
+                        GeometryReader { geometry in
+                            AsyncImage(url: URL(string: userImageURLString)) { phase in
+                                switch phase {
+                                case .success(let image):
+                                    image
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: geometry.size.width, height: geometry.size.height)
+                                        .clipShape(Circle())
+                                        .overlay(Circle().stroke(Color.white, lineWidth: 2))
+                                @unknown default:
+                                    EmptyView()
                                 }
                             }
-                            .frame(width: screenWidth * 0.6, height: screenWidth * 0.6)
-                            .padding(.bottom, 22)
-
-                            Text("Username:")
-                                    .font(.headline)
-                            Text(LoginViewModell.defaults.string(forKey: "username") ?? "")
-                                    .font(.subheadline)
-                                    .foregroundColor(.gray)
-                                    .padding(.bottom, 12)
-                            
-                            Text("Email:")
-                                    .font(.headline)
-                            Text(LoginViewModell.defaults.string(forKey: "email") ?? "")
-                                    .font(.subheadline)
-                                    .foregroundColor(.gray)
-                                    .padding(.bottom, 12)
-                            
-                            Text("First Name:")
-                                    .font(.headline)
-                            Text(LoginViewModell.defaults.string(forKey: "firstName") ?? "")
-                                    .font(.subheadline)
-                                    .foregroundColor(.gray)
-                                    .padding(.bottom, 12)
-                            
-                            Text("Last Name:")
-                                    .font(.headline)
-                            Text(LoginViewModell.defaults.string(forKey: "lastName") ?? "")
-                                    .font(.subheadline)
-                                    .foregroundColor(.gray)
-                                    .padding(.bottom, 12)
-                            
-                            Text("Role:")
-                                    .font(.headline)
-                            Text(LoginViewModell.defaults.string(forKey: "role") ?? "")
-                                    .font(.subheadline)
-                                    .foregroundColor(.gray)
-                                    .padding(.bottom, 12)
-                            
-                            Text("Total points:")
-                                    .font(.headline)
-                            Text(LoginViewModell.defaults.string(forKey: "nbPts") ?? "")
-                                    .font(.subheadline)
-                                    .foregroundColor(.gray)
-                                    .padding(.bottom, 12)
-
-                            
-                            
-                        
-                        
-                        
-                    ZStack {
-                        NavigationLink(destination: EditProfile(), isActive: $isActive) {
-                            
                         }
+                        .frame(width: screenWidth * 0.5, height: screenWidth * 0.5)
+                        .padding(.bottom, 10)
+                        
+                        Text("Username:")
+                            .font(.headline)
+                        Text(LoginViewModell.defaults.string(forKey: "username") ?? "")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                            .padding(.bottom, 12)
+                        
+                        Text("Email:")
+                            .font(.headline)
+                        Text(LoginViewModell.defaults.string(forKey: "email") ?? "")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                            .padding(.bottom, 12)
+                        
+                        Text("First Name:")
+                            .font(.headline)
+                        Text(LoginViewModell.defaults.string(forKey: "firstName") ?? "")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                            .padding(.bottom, 12)
+                        
+                        Text("Last Name:")
+                            .font(.headline)
+                        Text(LoginViewModell.defaults.string(forKey: "lastName") ?? "")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                            .padding(.bottom, 12)
+                        
+                        Text("Role:")
+                            .font(.headline)
+                        Text(LoginViewModell.defaults.string(forKey: "role") ?? "")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                            .padding(.bottom, 12)
+                        
+                        Text("Total points:")
+                            .font(.headline)
+                        Text(LoginViewModell.defaults.string(forKey: "nbPts") ?? "")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                            .padding(.bottom, 12)
+                        
+                        
+                        
+                        
+                        
+                        
+                        ZStack {
+                            NavigationLink(destination: EditProfile(), isActive: $isActive) {
+                                
+                            }
                             Button(action:{
                                 isActive = true
                             })
@@ -126,17 +126,18 @@ struct AccountScreen: View {
                                     .frame(minWidth: 0, maxWidth: .infinity)
                                     .frame(width: screenWidth * 0.88, height: screenWidth * 0.13)
                             }
-                    
+                            
                         }
                         .buttonStyle(.borderedProminent)
                         .buttonBorderShape(.roundedRectangle)
                         .tint(Color.blue)
                         .cornerRadius(30)
                         .padding(.horizontal)
-                            
+                        
                         
                         
                     }
+                }
             }
         }.navigationBarBackButtonHidden(true)
         .navigationBarColor(.darkBlue, textColor: UIColor.white)
